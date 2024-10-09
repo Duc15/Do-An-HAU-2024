69459436f22abd6928a83307ec18ae58c4442c0e
Duc15 <04phamduc@gmail.com>
2024-10-09 22:14:52 +0700

    giao dien login + sign up

---
+++ lib/methods/common_methods.dart
@@ -0,0 +1,22 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+
+class CommonMethods
+{
+  checkConnectivity(BuildContext context) async
+  {
+    var connectionResult = await Connectivity().checkConnectivity();
+
+    if(connectionResult != ConnectivityResult.mobile && connectionResult != ConnectivityResult.wifi)
+    {
+      if(!context.mounted) return;
+      displaySnackBar("Không thể dữ liệu từ máy chủ ! Vui lòng kiểm tra lại kết nối của bạn", context);
+    }
+  }
+
+  displaySnackBar(String messageText, BuildContext context)
+  {
+    var snackBar = SnackBar(content: Text(messageText));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
